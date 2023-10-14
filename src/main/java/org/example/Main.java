dadad12508c06e4056915c2065458a03d825e0c8
Minyoohyeon <trade0629@naver.com>
2023-10-14 17:47:48 +0900

    테스트 커밋

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("엄마 안녕?");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+        
+    }
+}
